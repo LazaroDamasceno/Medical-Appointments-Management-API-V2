181e362fc641625503fbf726603e99198cd85227
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-01 01:26:29 -0300

    Created the entity DoctorAuditTrail

---
+++ v2/src/main/java/com/api/v2/doctors/domain/DoctorAuditTrail.java
@@ -0,0 +1,29 @@
+package com.api.v2.doctors.domain;
+
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.bson.types.ObjectId;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+public record DoctorAuditTrail(
+        @BsonId
+        ObjectId id,
+        Doctor doctor,
+        LocalDateTime createdAt,
+        ZoneId createdAtZoneId,
+        ZoneOffset createdAtZoneOffset
+) {
+
+    public static DoctorAuditTrail create(Doctor doctor) {
+        return new DoctorAuditTrail(
+                new ObjectId(),
+                doctor,
+                LocalDateTime.now(),
+                ZoneId.systemDefault(),
+                OffsetDateTime.now().getOffset()
+        );
+    }
+}
