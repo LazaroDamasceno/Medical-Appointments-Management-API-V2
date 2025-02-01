327da8d2c419d7b084116c297cebbb4ef55f9035
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-01 01:56:13 -0300

    Created the util DoctorFinderUtil

---
+++ v2/src/main/java/com/api/v2/doctors/utils/DoctorFinderUtil.java
@@ -0,0 +1,30 @@
+package com.api.v2.doctors.utils;
+
+import com.api.v2.doctors.domain.Doctor;
+import com.api.v2.doctors.domain.DoctorRepository;
+import com.api.v2.doctors.exceptions.NonExistentDoctorException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class DoctorFinderUtil {
+
+    private final DoctorRepository doctorRepository;
+
+    public DoctorFinderUtil(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+    public Doctor findByMedicalLicenseNumber(String medicalLicenseNumber) {
+        Optional<Doctor> optionalDoctor = doctorRepository
+                .findAll()
+                .stream()
+                .filter(d -> d.getMedicalLicenseNumber().equals(medicalLicenseNumber))
+                .findFirst();
+        if (optionalDoctor.isEmpty()) {
+            throw new NonExistentDoctorException(medicalLicenseNumber);
+        }
+        return optionalDoctor.get();
+    }
+}
