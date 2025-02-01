5f2740b2e87bc07ea7fb8758bce69ddd35759bfc
lazarodamasceno <lazarolawrence2@gmail.co>
2025-01-31 23:57:27 -0300

    Enabled the Virtual Thread

---
+++ v3/src/main/java/com/api/v3/people/dtos/package-info.java
@@ -0,0 +1,7 @@
+@ApplicationModule(
+        type = ApplicationModule.Type.OPEN
+)
+
+package com.api.v3.people.dtos;
+
+import org.springframework.modulith.ApplicationModule;
---
+++ v3/src/main/java/com/api/v3/people/services/interfaces/package-info.java
@@ -0,0 +1,6 @@
+@ApplicationModule(
+        type = ApplicationModule.Type.OPEN
+)
+package com.api.v3.people.services.interfaces;
+
+import org.springframework.modulith.ApplicationModule;
