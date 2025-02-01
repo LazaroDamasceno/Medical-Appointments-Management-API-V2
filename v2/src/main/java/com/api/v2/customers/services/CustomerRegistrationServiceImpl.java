4ed14049a9f7f64151761807f4c980a3e3255a9a
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-01 02:09:58 -0300

    Added checkers to CustomerRegistrationServiceImpl

--- v2/src/main/java/com/api/v2/customers/services/CustomerRegistrationServiceImpl.java
+++ v2/src/main/java/com/api/v2/customers/services/CustomerRegistrationServiceImpl.java
@@ -6,6 +6,8 @@ import com.api.v2.customers.dtos.CustomerRegistrationDto;
 import com.api.v2.customers.dtos.CustomerResponseDto;
 import com.api.v2.customers.utils.CustomerResponseMapper;
 import com.api.v2.people.domain.Person;
+import com.api.v2.people.exceptions.DuplicatedEmailException;
+import com.api.v2.people.exceptions.DuplicatedSsnException;
 import com.api.v2.people.services.interfaces.PersonRegistrationService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
@@ -25,9 +27,31 @@ public class CustomerRegistrationServiceImpl implements CustomerRegistrationServ
 
     @Override
     public CustomerResponseDto register(@Valid CustomerRegistrationDto registrationDto) {
+        onDuplicatedSsn(registrationDto.personRegistrationDto().ssn());
+        onDuplicatedEmail(registrationDto.personRegistrationDto().email());
         Person savedPerson = personRegistrationService.register(registrationDto.personRegistrationDto());
         Customer customer = Customer.create(registrationDto.address(), savedPerson);
         Customer savedCustomer = customerRepository.save(customer);
         return CustomerResponseMapper.mapToDto(savedCustomer);
+    }
+
+    private void onDuplicatedSsn(String ssn) {
+        boolean isSsnDuplicated = customerRepository
+                .findAll()
+                .stream()
+                .anyMatch(c -> c.getPerson().getSsn().equals(ssn));
+        if (isSsnDuplicated) {
+            throw new DuplicatedSsnException();
+        }
+    }
+
+    private void onDuplicatedEmail(String email) {
+        boolean isEmailDuplicated = customerRepository
+                .findAll()
+                .stream()
+                .anyMatch(c -> c.getPerson().getEmail().equals(email));
+        if (isEmailDuplicated) {
+            throw new DuplicatedEmailException();
+        }
     }
 }
