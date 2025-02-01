e4bdbfe9d65634000d8c6b537e430bc1c9b801e9
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-01 02:10:06 -0300

    Added checkers to DoctorHiringServiceImpl

--- v2/src/main/java/com/api/v2/doctors/services/DoctorHiringServiceImpl.java
+++ v2/src/main/java/com/api/v2/doctors/services/DoctorHiringServiceImpl.java
@@ -4,8 +4,11 @@ import com.api.v2.doctors.domain.Doctor;
 import com.api.v2.doctors.domain.DoctorRepository;
 import com.api.v2.doctors.dto.DoctorHiringDto;
 import com.api.v2.doctors.dto.DoctorResponseDto;
+import com.api.v2.doctors.exceptions.DuplicatedMedicalLicenseNumberException;
 import com.api.v2.doctors.utils.DoctorResponseMapper;
 import com.api.v2.people.domain.Person;
+import com.api.v2.people.exceptions.DuplicatedEmailException;
+import com.api.v2.people.exceptions.DuplicatedSsnException;
 import com.api.v2.people.services.interfaces.PersonRegistrationService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
@@ -25,9 +28,42 @@ public class DoctorHiringServiceImpl implements DoctorHiringService {
 
     @Override
     public DoctorResponseDto hire(@Valid DoctorHiringDto hiringDto) {
+        onDuplicatedSsn(hiringDto.personRegistrationDto().ssn());
+        onDuplicatedEmail(hiringDto.personRegistrationDto().email());
+        onDuplicatedMedicalLicenseNumber(hiringDto.medicalLicenseNumber());
         Person savedPerson = personRegistrationService.register(hiringDto.personRegistrationDto());
         Doctor doctor = Doctor.create(savedPerson, hiringDto.medicalLicenseNumber());
         Doctor savedDoctor = doctorRepository.save(doctor);
         return DoctorResponseMapper.mapToDto(savedDoctor);
+    }
+
+    private void onDuplicatedSsn(String ssn) {
+        boolean isSsnDuplicated = doctorRepository
+                .findAll()
+                .stream()
+                .anyMatch(c -> c.getPerson().getSsn().equals(ssn));
+        if (isSsnDuplicated) {
+            throw new DuplicatedSsnException();
+        }
+    }
+
+    private void onDuplicatedEmail(String email) {
+        boolean isEmailDuplicated = doctorRepository
+                .findAll()
+                .stream()
+                .anyMatch(c -> c.getPerson().getEmail().equals(email));
+        if (isEmailDuplicated) {
+            throw new DuplicatedEmailException();
+        }
+    }
+
+    private void onDuplicatedMedicalLicenseNumber(String medicalLicenseNumber) {
+        boolean isMedicalLicenseNumberDuplicated = doctorRepository
+                .findAll()
+                .stream()
+                .anyMatch(d -> d.getMedicalLicenseNumber().equals(medicalLicenseNumber));
+        if (isMedicalLicenseNumberDuplicated) {
+            throw new DuplicatedMedicalLicenseNumberException();
+        }
     }
 }
