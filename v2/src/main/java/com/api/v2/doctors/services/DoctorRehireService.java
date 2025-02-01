95c96f131ecf73468251c86c04187c6540a1e78b
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-01 18:48:42 -0300

    Created the service DoctorRehireService

---
+++ v2/src/main/java/com/api/v2/doctors/services/DoctorRehireService.java
@@ -0,0 +1,5 @@
+package com.api.v2.doctors.services;
+
+public interface DoctorRehireService {
+    void rehire(String medicalLicenseNumber);
+}
---
+++ v2/src/main/java/com/api/v2/doctors/services/DoctorRehireServiceImpl.java
@@ -0,0 +1,43 @@
+package com.api.v2.doctors.services;
+
+import com.api.v2.doctors.domain.Doctor;
+import com.api.v2.doctors.domain.DoctorAuditTrail;
+import com.api.v2.doctors.domain.DoctorAuditTrailRepository;
+import com.api.v2.doctors.domain.DoctorRepository;
+import com.api.v2.doctors.exceptions.ImmutableDoctorStatusException;
+import com.api.v2.doctors.utils.DoctorFinderUtil;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DoctorRehireServiceImpl implements DoctorRehireService {
+
+    private final DoctorFinderUtil doctorFinderUtil;
+    private final DoctorRepository doctorRepository;
+    private final DoctorAuditTrailRepository doctorAuditTrailRepository;
+
+    public DoctorRehireServiceImpl(DoctorFinderUtil doctorFinderUtil,
+                                        DoctorRepository doctorRepository,
+                                        DoctorAuditTrailRepository doctorAuditTrailRepository
+    ) {
+        this.doctorFinderUtil = doctorFinderUtil;
+        this.doctorRepository = doctorRepository;
+        this.doctorAuditTrailRepository = doctorAuditTrailRepository;
+    }
+
+    @Override
+    public void rehire(String medicalLicenseNumber) {
+        Doctor doctor = doctorFinderUtil.findByMedicalLicenseNumber(medicalLicenseNumber);
+        onActiveDoctor(doctor);
+        DoctorAuditTrail doctorAuditTrail = DoctorAuditTrail.create(doctor);
+        doctorAuditTrailRepository.save(doctorAuditTrail);
+        doctor.markAsRehired();
+        doctorRepository.save(doctor);
+    }
+
+    private void onActiveDoctor(Doctor doctor) {
+        if (doctor.getTerminatedAt() == null) {
+            String message = "Doctor whose medical license number is %s is active.";
+            throw new ImmutableDoctorStatusException(message);
+        }
+    }
+}
