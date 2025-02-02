00ed4c64fa80b0de88fcec94df0d51f050e9c888
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-02 02:06:33 -0300

    Fixed CustomerResponseMapper

--- v2/src/main/java/com/api/v2/customers/utils/CustomerResponseMapper.java
+++ v2/src/main/java/com/api/v2/customers/utils/CustomerResponseMapper.java
@@ -1,5 +1,6 @@
 package com.api.v2.customers.utils;
 
+import com.api.v2.common.AddressResponseMapper;
 import com.api.v2.customers.domain.Customer;
 import com.api.v2.customers.dtos.CustomerResponseDto;
 import com.api.v2.people.utils.PersonResponseMapper;
@@ -8,7 +9,7 @@ public class CustomerResponseMapper {
     public static CustomerResponseDto mapToDto(Customer customer) {
         return new CustomerResponseDto(
                 PersonResponseMapper.mapToDto(customer.getPerson()),
-                customer.getAddress()
+                AddressResponseMapper.mapToString(customer.getAddress())
         );
     }
 }
