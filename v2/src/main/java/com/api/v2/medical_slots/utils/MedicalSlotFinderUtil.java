213faadf8cae78c3013f580daf97c78a55085d00
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-02 17:41:45 -0300

    Created the util MedicalSlotFinderUtil

---
+++ v2/src/main/java/com/api/v2/medical_slots/utils/MedicalSlotFinderUtil.java
@@ -0,0 +1,27 @@
+package com.api.v2.medical_slots.utils;
+
+import com.api.v2.medical_slots.domain.MedicalSlot;
+import com.api.v2.medical_slots.domain.MedicalSlotRepository;
+import com.api.v2.medical_slots.exceptions.NonExistentMedicalSlotException;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class MedicalSlotFinderUtil {
+
+    private final MedicalSlotRepository medicalSlotRepository;
+
+    public MedicalSlotFinderUtil(MedicalSlotRepository medicalSlotRepository) {
+        this.medicalSlotRepository = medicalSlotRepository;
+    }
+
+    public MedicalSlot findById(String id) {
+        Optional<MedicalSlot> optionalMedicalSlot = medicalSlotRepository.findById(new ObjectId(id));
+        if (optionalMedicalSlot.isEmpty()) {
+            throw new NonExistentMedicalSlotException(id);
+        }
+        return optionalMedicalSlot.get();
+    }
+}
