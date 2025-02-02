bf2237e282f46676e2c52696b86afde99af28195
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-02 02:59:34 -0300

    Added a private checker onTerminatedDoctor to DoctorModificationServiceImpl

--- v2/src/main/java/com/api/v2/doctors/services/DoctorModificationServiceImpl.java
+++ v2/src/main/java/com/api/v2/doctors/services/DoctorModificationServiceImpl.java
@@ -3,6 +3,7 @@ package com.api.v2.doctors.services;
 import com.api.v2.doctors.controller.DoctorController;
 import com.api.v2.doctors.domain.Doctor;
 import com.api.v2.doctors.domain.DoctorRepository;
+import com.api.v2.doctors.exceptions.ImmutableDoctorStatusException;
 import com.api.v2.doctors.resources.DoctorResponseResource;
 import com.api.v2.doctors.utils.DoctorFinderUtil;
 import com.api.v2.doctors.utils.DoctorResponseMapper;
@@ -34,6 +35,7 @@ public class DoctorModificationServiceImpl implements DoctorModificationService 
     @Override
     public EntityModel<DoctorResponseResource> modify(String medicalLicenseNumber, PersonModificationDto modificationDto) {
         Doctor doctor = doctorFinderUtil.findByMedicalLicenseNumber(medicalLicenseNumber);
+        onTerminatedDoctor(doctor);
         Person modifiedPerson = personModificationService.modify(doctor.getPerson(), modificationDto);
         doctor.setPerson(modifiedPerson);
         Doctor modifiedDoctor = doctorRepository.save(doctor);
@@ -54,5 +56,14 @@ public class DoctorModificationServiceImpl implements DoctorModificationService 
                             methodOn(DoctorController.class).findAll()
                     ).withRel("find_all_doctors")
                 );
+    }
+
+    private void onTerminatedDoctor(Doctor doctor) {
+        if (doctor.getTerminatedAt() != null) {
+            String message = """
+                    Doctor whose medical license number is %s cannot be modified, because they're terminated.
+            """.formatted(doctor.getMedicalLicenseNumber());
+            throw new ImmutableDoctorStatusException(message);
+        }
     }
 }
