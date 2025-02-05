6c7535612b6b2f3a3b112e11e77296178b9ffbee
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-05 00:20:15 -0300

    Created the util MedicalAppointmentResponseMapper

---
+++ v2/src/main/java/com/api/v2/medical_appointments/utils/MedicalAppointmentResponseMapper.java
@@ -0,0 +1,106 @@
+package com.api.v2.medical_appointments.utils;
+
+import com.api.v2.customers.utils.CustomerResponseMapper;
+import com.api.v2.doctors.utils.DoctorResponseMapper;
+import com.api.v2.medical_appointments.domain.MedicalAppointment;
+import com.api.v2.medical_appointments.dtos.SimplifiedMedicalAppointmentResponseDto;
+import com.api.v2.medical_appointments.resources.MedicalAppointmentResponseResource;
+
+public class MedicalAppointmentResponseMapper {
+
+    public static MedicalAppointmentResponseResource mapToResource(MedicalAppointment medicalAppointment) {
+        if (medicalAppointment.getCompletedAt() != null && medicalAppointment.getCanceledAt() == null) {
+            return new MedicalAppointmentResponseResource(
+                    medicalAppointment.getId().toString(),
+                    CustomerResponseMapper.mapToResource(medicalAppointment.getCustomer()),
+                    DoctorResponseMapper.mapToResource(medicalAppointment.getDoctor()),
+                    "%s%s[%s]".formatted(
+                            medicalAppointment.getBookedAt(),
+                            medicalAppointment.getBookedAtZoneOffset(),
+                            medicalAppointment.getBookedAtZoneId()
+                    ),
+                    null,
+                    "%s%s[%s]".formatted(
+                            medicalAppointment.getCompletedAt(),
+                            medicalAppointment.getCompletedAtZoneOffset(),
+                            medicalAppointment.getCompletedAtZoneId()
+                    )
+            );
+        }
+        if (medicalAppointment.getCompletedAt() == null && medicalAppointment.getCanceledAt() != null) {
+            return new MedicalAppointmentResponseResource(
+                    medicalAppointment.getId().toString(),
+                    CustomerResponseMapper.mapToResource(medicalAppointment.getCustomer()),
+                    DoctorResponseMapper.mapToResource(medicalAppointment.getDoctor()),
+                    "%s%s[%s]".formatted(
+                            medicalAppointment.getBookedAt(),
+                            medicalAppointment.getBookedAtZoneOffset(),
+                            medicalAppointment.getBookedAtZoneId()
+                    ),
+                    "%s%s[%s]".formatted(
+                            medicalAppointment.getCanceledAt(),
+                            medicalAppointment.getCanceledAtZoneOffset(),
+                            medicalAppointment.getCanceledAtZoneId()
+                    ),
+                    null
+            );
+        }
+        return new MedicalAppointmentResponseResource(
+                medicalAppointment.getId().toString(),
+                CustomerResponseMapper.mapToResource(medicalAppointment.getCustomer()),
+                DoctorResponseMapper.mapToResource(medicalAppointment.getDoctor()),
+                "%s%s[%s]".formatted(
+                        medicalAppointment.getBookedAt(),
+                        medicalAppointment.getBookedAtZoneOffset(),
+                        medicalAppointment.getBookedAtZoneId()
+                ),
+                null,
+                null
+        );
+    }
+
+    public static SimplifiedMedicalAppointmentResponseDto mapToDto(MedicalAppointment medicalAppointment) {
+        if (medicalAppointment.getCompletedAt() == null && medicalAppointment.getCanceledAt() != null) {
+            return new SimplifiedMedicalAppointmentResponseDto(
+                    medicalAppointment.getId().toString(),
+                    "%s%s[%s]".formatted(
+                            medicalAppointment.getBookedAt(),
+                            medicalAppointment.getBookedAtZoneOffset(),
+                            medicalAppointment.getBookedAtZoneId()
+                    ),
+                    "%s%s[%s]".formatted(
+                            medicalAppointment.getCanceledAt(),
+                            medicalAppointment.getCanceledAtZoneOffset(),
+                            medicalAppointment.getCanceledAtZoneId()
+                    ),
+                    null
+            );
+        }
+        if (medicalAppointment.getCompletedAt() != null && medicalAppointment.getCanceledAt() == null) {
+            return new SimplifiedMedicalAppointmentResponseDto(
+                    medicalAppointment.getId().toString(),
+                    "%s%s[%s]".formatted(
+                            medicalAppointment.getBookedAt(),
+                            medicalAppointment.getBookedAtZoneOffset(),
+                            medicalAppointment.getBookedAtZoneId()
+                    ),
+                    null,
+                    "%s%s[%s]".formatted(
+                            medicalAppointment.getCompletedAt(),
+                            medicalAppointment.getCompletedAtZoneOffset(),
+                            medicalAppointment.getCompletedAtZoneId()
+                    )
+            );
+        }
+        return new SimplifiedMedicalAppointmentResponseDto(
+                medicalAppointment.getId().toString(),
+                "%s%s[%s]".formatted(
+                        medicalAppointment.getBookedAt(),
+                        medicalAppointment.getBookedAtZoneOffset(),
+                        medicalAppointment.getBookedAtZoneId()
+                ),
+                null,
+                null
+        );
+    }
+}
