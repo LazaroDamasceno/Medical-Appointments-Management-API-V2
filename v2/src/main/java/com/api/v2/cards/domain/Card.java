856499b26887412711e07ec2d0eab7abb0de5a44
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-07 17:58:22 -0300

    Created the entity Card

---
+++ v2/src/main/java/com/api/v2/cards/domain/Card.java
@@ -0,0 +1,31 @@
+package com.api.v2.cards.domain;
+
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.*;
+
+@Document
+public record Card(
+        @BsonId
+        ObjectId id,
+        String bank,
+        String cvc_cvv,
+        LocalDate dueDate,
+        LocalDateTime createdAt,
+        ZoneId createdAtZoneId,
+        ZoneOffset createdAtZoneOffset
+) {
+
+    public static Card create(String bank, String cvc_cvv, LocalDate dueDate) {
+        return new Card(
+                new ObjectId(),
+                bank, cvc_cvv,
+                dueDate,
+                LocalDateTime.now(),
+                ZoneId.systemDefault(),
+                OffsetTime.now().getOffset()
+        );
+    }
+}
