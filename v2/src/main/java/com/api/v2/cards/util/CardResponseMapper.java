979be0a7a82326a50c28441bb69536efa88fda13
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-07 21:17:29 -0300

    Created the util CardResponseMapper

---
+++ v2/src/main/java/com/api/v2/cards/util/CardResponseMapper.java
@@ -0,0 +1,16 @@
+package com.api.v2.cards.util;
+
+import com.api.v2.cards.domain.Card;
+import com.api.v2.cards.dtos.CardResponseDto;
+
+public class CardResponseMapper {
+    public static CardResponseDto map(Card card) {
+        return new CardResponseDto(
+                card.id().toString(),
+                card.type(),
+                card.bank(),
+                card.cvc_cvv(),
+                card.dueDate()
+        );
+    }
+}
