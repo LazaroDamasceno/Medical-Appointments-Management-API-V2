b3bf0eab29bdd41751556c79aece277e3ca8ac3f
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-07 21:21:12 -0300

    Created the util CardFinderUtil

---
+++ v2/src/main/java/com/api/v2/cards/util/CardFinderUtil.java
@@ -0,0 +1,23 @@
+package com.api.v2.cards.util;
+
+import com.api.v2.cards.domain.Card;
+import com.api.v2.cards.domain.CardRepository;
+import com.api.v2.cards.exceptions.NonExistentCardException;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardFinderUtil {
+
+    private final CardRepository cardRepository;
+
+    public CardFinderUtil(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    public Card findById(String id) {
+        return cardRepository
+                .findById(new ObjectId(id))
+                .orElseThrow(() -> new NonExistentCardException(id));
+    }
+}
