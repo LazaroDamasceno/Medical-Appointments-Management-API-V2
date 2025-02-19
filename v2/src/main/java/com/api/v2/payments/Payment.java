970ce9cbad78a8326a68c2b0cf1b0f41df2a451f
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-19 19:26:48 -0300

    Created the entity Payment

---
+++ v2/src/main/java/com/api/v2/payments/Payment.java
@@ -0,0 +1,41 @@
+package com.api.v2.payments;
+
+import com.api.v2.cards.domain.Card;
+import com.api.v2.common.DstCheckerUtil;
+import com.api.v2.medical_appointments.domain.MedicalAppointment;
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+@Document
+public record Payment(
+        @BsonId
+        ObjectId id,
+        Card card,
+        MedicalAppointment medicalAppointment,
+        LocalDateTime paidAt,
+        ZoneId paidAtZoneId,
+        ZoneOffset paidAtZoneOffset,
+        boolean isPaymentDuringDST
+) {
+
+    public static Payment create(Card card, MedicalAppointment medicalAppointment) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        ZoneId zoneId = ZoneId.systemDefault();
+        ZoneOffset zoneOffset = OffsetDateTime.now().getOffset();
+        return new Payment(
+                new ObjectId(),
+                card,
+                medicalAppointment,
+                localDateTime,
+                zoneId,
+                zoneOffset,
+                DstCheckerUtil.isGivenDateTimeFollowingDst(localDateTime, zoneId)
+        );
+    }
+}
