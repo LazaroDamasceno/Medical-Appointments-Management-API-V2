57e1c037b01df944aeb3e5ef057cfcb724c1bc7d
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-27 18:34:53 -0300

    Created the class Constants

---
+++ v2/src/main/java/com/api/v2/common/Constants.java
@@ -0,0 +1,11 @@
+package com.api.v2.common;
+
+public class Constants {
+
+    public static final int OK_200 = 200;
+    public static final int CREATED_201 = 201;
+    public static final int NO_CONTENT_204 = 204;
+    public static final int NOT_FOUND_404 = 404;
+    public static final int CONFLICT_409 = 409;
+
+}
