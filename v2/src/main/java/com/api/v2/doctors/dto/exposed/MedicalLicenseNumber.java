c51f42030e70ade88961706c3e8a97320f1bc61b
lazarodamasceno <lazarolawrence2@gmail.co>
2025-03-17 04:57:54 -0300

    Changed MedicalLicenseNumber

--- v2/src/main/java/com/api/v2/doctors/dto/exposed/MedicalLicenseNumber.java
+++ v2/src/main/java/com/api/v2/doctors/dto/exposed/MedicalLicenseNumber.java
@@ -3,9 +3,10 @@ package com.api.v2.doctors.dto.exposed;
 import com.api.v2.doctors.enums.Regions;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record MedicalLicenseNumber(
-        @NotBlank String licenseNumber,
+        @NotBlank @Size(min = 8, max = 9) String licenseNumber,
         @NotNull Regions region
 ) {
 }
