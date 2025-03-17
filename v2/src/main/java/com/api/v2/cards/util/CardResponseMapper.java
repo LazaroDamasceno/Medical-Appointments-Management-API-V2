295e47ca8bffaf3a3c31ba750e1da540875eb006
lazarodamasceno <lazarolawrence2@gmail.co>
2025-03-17 01:36:07 -0300

    Changed CardResponseMapper

--- v2/src/main/java/com/api/v2/cards/util/CardResponseMapper.java
+++ v2/src/main/java/com/api/v2/cards/util/CardResponseMapper.java
@@ -6,7 +6,7 @@ import com.api.v2.cards.dtos.CardResponseDto;
 public final class CardResponseMapper {
     public static CardResponseDto map(Card card) {
         return new CardResponseDto(
-                card.id().toString(),
+                card.id(),
                 card.type(),
                 card.bank(),
                 card.cvc_cvv(),
